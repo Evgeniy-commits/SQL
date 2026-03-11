3ff490ac8dfb94ecd7fbf5a5feb4f4d4f719f7ff
Admin <Admin@DESKTOP-8URQBFL>
2026-03-11 23:52:26 +0500

    InsertSchedule1221 in PROGRESS

--- StoredProcedures/SQLQuery0-Check.sql
+++ StoredProcedures/SQLQuery0-Check.sql
@@ -2,7 +2,10 @@
 USE PV_521_Import;
 SET DATEFIRST 1;
 
+--DELETE FROM Schedule WHERE [group] = 521;
 --DELETE FROM Schedule WHERE discipline = (SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE N'Сетевое%');
 --EXEC sp_InsertScheduleStacionar N'PV_521', N'%ADO.NET%', N'Олег', N'2026-01-21';
-EXEC sp_InsertScheduleStacionar N'PV_521', N'Сетевое%', N'Олег', N'2026-02-18';
+EXEC sp_InsertSchedule1221 N'PV_521', N'Hardware%', N'Свищев', N'2025-01-20', 1, 3, 5;
+EXEC sp_InsertSchedule1221 N'PV_521', N'%Windows', N'Свищев', N'2025-04-30', 1, 3, 5;
+EXEC sp_InsertSchedule1221 N'PV_521', N'Процедурное%C++', N'Ковтун', N'2025-01-20', 5, 3, 1;
 EXEC sp_SelectScheduleFor N'PV_521';
